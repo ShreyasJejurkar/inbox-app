221aad89ca00573caf103a45c3e1e8dca61ba0c5
Shreyas Jejurkar <shreyasjejurkar123@live.com>
2022-04-17 12:23:00 +0530

    connect to astra db sucessfully!

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,10 +1,18 @@
-﻿using Microsoft.AspNetCore.Authentication;
+﻿using Cassandra;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc;
 
 namespace inbox_app.Controllers
 {
     public class HomeController : Controller
     {
+        private IWebHostEnvironment _webHostEnvironment;
+
+        public HomeController(IWebHostEnvironment webHostEnvironment)
+        {
+            _webHostEnvironment = webHostEnvironment;
+        }
+
         [HttpGet]
         public IActionResult Index()
         {
@@ -39,6 +47,26 @@ namespace inbox_app.Controllers
                     RedirectUri = Url.IsLocalUrl(ViewBag.ReturnUrl) ? ViewBag.ReturnUrl : "/"
                 }, provider);
         }
+
+
+        [HttpGet]
+        public IActionResult Connect()
+        {
+            var path = Path.Combine(_webHostEnvironment.WebRootPath, "db", "secure-connect-inbox-app-db.zip");
+
+            var cluster = Cluster.Builder()
+                .WithCloudSecureConnectionBundle(path)
+                .WithCredentials("NOpZWGaMNZLOQkzWLhGTBxMu", "Ov-lG08,Lz6Nlb1BK9,,XUut3p5GeQsmxCczRXaaavPJIfCJaiUcZ.Fd813DLZvYe4lbZgUPBcn14Y2RtOgh.oDl3uaFWTIbYFY0yhn_aQR7JMd3C5w0OPmfO8NvFayE")
+                .Build();
+
+            var session = cluster.Connect("main");
+
+
+            return null;
+        }
+
+
+
 
         private static async Task<AuthenticationScheme[]> GetExternalProvidersAsync(HttpContext context)
         {
